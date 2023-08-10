7d8293145b3852cba930ced42420a36de7ec882b
Md. Mamun <mkmamun031@gmail.com>
2023-08-10 21:13:01 +0600

    Added Login Page

--- AmarDaan/static/index.js
+++ AmarDaan/static/index.js
@@ -1,4 +1,6 @@
-// JavaScript code here
+// main.js
+
+// Code for changing header image
 const headerImage = document.getElementById("headerImage");
 const imageSources = [
   "./resources/images/header-img-1.jpg",
@@ -17,13 +19,19 @@ function changeImage() {
 
 setInterval(changeImage, 3000);
 
+// Code for campaign cards
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 const cardRow = document.getElementById("cardRow");
 
 const campaignData = [
-  // Your campaign data objects here
-];
+    { name: "Campaign 1", budget: 1000, collected: 750, imageUrl: "./resources/images/header-img-1.jpg" },
+    { name: "Campaign 2", budget: 2000, collected: 1500, imageUrl: "./resources/images/header-img-4.jpg" },
+    { name: "Campaign 2", budget: 2000, collected: 1500, imageUrl: "./resources/images/header-img-5.jpg" },
+    { name: "Campaign 2", budget: 2000, collected: 1500, imageUrl: "./resources/images/header-img-6.jpg" },
+    { name: "Campaign 2", budget: 2000, collected: 1500, imageUrl: "./resources/images/header-img-3.jpg" },
+    // Add more campaign data objects as needed
+  ];
 
 let startIndex = 0;
 const cardsPerRow = 4;
@@ -37,20 +45,18 @@ nextBtn.addEventListener("click", () => {
   startIndex = Math.min(startIndex + cardsPerRow, campaignData.length - cardsPerRow);
   updateCards();
 });
-// JavaScript code here
-// ...
 
 function updateCards() {
     const endIndex = startIndex + cardsPerRow;
     cardRow.innerHTML = "";
-  
+
     for (let i = startIndex; i < endIndex; i++) {
       if (i >= campaignData.length) {
         break;
       }
-  
+
       const progressPercentage = (campaignData[i].collected / campaignData[i].budget) * 100;
-  
+
       const card = document.createElement("div");
       card.className = "col";
       card.innerHTML = `
@@ -68,10 +74,22 @@ function updateCards() {
           </div>
         </div>
       `;
-  
+
       cardRow.appendChild(card);
     }
   }
-  
-  updateCards();
-  
+
+updateCards();
+
+// Code for sign-up/sign-in buttons
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
