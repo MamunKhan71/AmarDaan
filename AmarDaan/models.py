9210fbc0010fd09fe541d7afd29a594e8cbbb62b
Md. Mamun <mkmamun031@gmail.com>
2023-08-14 00:20:18 +0600

    Code Improvements!

--- AmarDaan/models.py
+++ AmarDaan/models.py
@@ -4,9 +4,25 @@ from sqlalchemy.sql import func
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key= True)
-    data = db.Column(db.String(10000))
+    camp_name = db.Column(db.String(200))
+    camp_category = db.Column(db.String(200))
+    camp_division = db.Column(db.String(200))
+    camp_zilla = db.Column(db.String(200))
+    camp_upzilla = db.Column(db.String(200))
+    camp_payment = db.Column(db.String(200))
+    camp_mobile = db.Column(db.String(200))    
+    camp_deadline = db.Column(db.String(200))
+    camp_story = db.Column(db.String(200))
+    camp_photo = db.Column(db.String(200))
+    camp_gender = db.Column(db.String(200))
+    camp_age = db.Column(db.String(200))
+    camp_occupation = db.Column(db.String(200))
+    camp_video = db.Column(db.String(200))
+    camp_social = db.Column(db.String(200))
+    camp_aboutus = db.Column(db.String(200))
     date = db.Column(db.DateTime(timezone=True), default= func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
