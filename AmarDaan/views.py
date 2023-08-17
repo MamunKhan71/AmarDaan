07d51806f780062a23f9a6ebf57820de5353d178
Md. Mamun <mkmamun031@gmail.com>
2023-08-17 23:01:32 +0600

    Code Improvements!

--- AmarDaan/views.py
+++ AmarDaan/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, current_app, redirect, url_for
+from flask import Blueprint, render_template, request, current_app, redirect, url_for, session
 from flask_login import login_required, current_user
 from .models import Campaign
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,6 +9,7 @@ import smtplib
 from email.mime.text import MIMEText
 import random
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 @login_required
@@ -112,6 +113,26 @@ def upload_profile_picture():
 
     return redirect(url_for('views.dashboard'))
 
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipients
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message Sent!")
+
+def otp_verification():
+    otp = random.randint(pow(10, 5), (pow(10, 6) - 1))
+    subject = "Your OTP Verification Code"
+    body = f"Dear {current_user.name},\n\nThank you for choosing AmarDaan! To ensure the security of your account, we have sent you a One-Time Password (OTP) for verification purposes. \n\nYour OTP: {otp} \n\nPlease enter this OTP on the verification page to complete the process. Please note that this OTP is valid for a limited time and should not be shared with anyone.\n\nIf you did not initiate this request or have any concerns, please contact our customer support immediately at Customer Support amardaan247@gmail.com. \n\nThank you for your trust in AmarDaan."
+    sender = "amardaan247@gmail.com"
+    recipients = current_user.email
+    password = "ecaflcbwzegogtnr"
+    send_email(subject, body, sender, recipients, password)
+    return otp
+
 @views.route('/update_profile', methods=["POST"])
 @login_required
 def update_profile():
@@ -122,43 +143,49 @@ def update_profile():
     facebook = request.form.get('facebook')
     instagram = request.form.get('instagram')
     updated_name = f"{first_name} {last_name}"
-    user=current_user
-    if updated_name:
-        user.name = updated_name
-    if email:
-        user.email = email
-    if password:
-        user.password = generate_password_hash(password=password, method='sha256')
-    if facebook:    
-        user.facebook = f"https://www.fb.com/{facebook}"
-    if instagram:
-        user.instagram = f"https://www.instagram.com/{instagram}"
-    db.session.commit()
-    return render_template('otp_page.html', user=current_user)
 
-@views.route('/otp_verification', methods=["GET", "POST"])
+    # Generate and store OTP
+    generated_otp = otp_verification()
+    session['generated_otp'] = generated_otp
+
+    return render_template('otp_page.html', user=current_user, otp=generated_otp)
+
+
+@views.route('/otp_verified', methods=['POST'])
 @login_required
-def otp_verification():
-    otp_code = random.randint(pow(10, 4), (pow(10, 5) - 1))
-    def send_email(subject, body, sender, recepients, password):
-        msg = MIMEText(body)
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['To'] = ','.join(recepients)
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-            smtp_server.login(sender, password)
-            smtp_server.sendmail(sender, recepients, msg.as_string())
-        print("Message Sent!")
+def otp_verified():
+    user_otp = f"{request.form.get('first')}{request.form.get('second')}{request.form.get('third')}{request.form.get('fourth')}{request.form.get('fifth')}{request.form.get('sixth')}"
+    generated_otp = session.get('generated_otp')
 
-    subject = "Your OTP Verification Code"
-    body = f" Dear {current_user.name},\nThank you for choosing AmarDaan! To ensure the security of your account, we have sent you a One-Time Password (OTP) for verification purposes. \nYour OTP: {otp_code} \nPlease enter this OTP on the verification page to complete the process. Please note that this OTP is valid for a limited time and should not be shared with anyone.\nIf you did not initiate this request or have any concerns, please contact our customer support immediately at [Customer Support amardaan247@gmail.com. \nThank you for your trust in AmarDaan."
-    sender = "amardaan247@gmail.com"
-    recepients = current_user.email
-    password = "AtgsWbics153523?"
-    send_email(subject, body, sender, recepients, password)
+    if user_otp == generated_otp:
+        try:
+            user = current_user
 
+            # Retrieve updated_name, email, etc. from the form
+            updated_name = f"{request.form.get('first_name')} {request.form.get('last_name')}"
+            email = request.form.get('email')
+            password = request.form.get('password')
+            facebook = request.form.get('facebook')
+            instagram = request.form.get('instagram')
 
-    
+            if updated_name:
+                user.name = updated_name
+            if email:
+                user.email = email
+            if password:
+                user.password = generate_password_hash(password=password, method='sha256')
+            if facebook:    
+                user.facebook = f"https://www.fb.com/{facebook}"
+            if instagram:
+                user.instagram = f"https://www.instagram.com/{instagram}"
 
+            db.session.commit()
+            print("Profile Updated Successfully!")
+        except Exception as e:
+            print("Error:", str(e))
+            db.session.rollback()
+            return render_template('error.html', message="An error occurred while saving data.")
+    else:
+        return render_template("error.html", message="ERROR 404 - WRONG OTP")
 
 
