ce53056f306945bb1b82e039eed26d2c96268473
Md. Mamun <mkmamun031@gmail.com>
2023-10-13 01:28:00 +0600

    Working on payment gateway integration!

--- AmarDaan/auth.py
+++ AmarDaan/auth.py
@@ -1,25 +1,26 @@
-from flask import Blueprint , render_template , request , flash , redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
+
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form.get('email')
         passw = request.form.get('password')
-        
+
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, passw):
-               flash("Account Logged Succesfully!", category='success')
-               login_user(user=user, remember=True)
-               return redirect(url_for('views.home'))
+                flash("Account Logged Succesfully!", category='success')
+                login_user(user=user, remember=True)
+                return redirect(url_for('views.home'))
             else:
-               flash("Incorrect Password!", category='error')
-
+                flash("Incorrect Password!", category='error')
 
         return redirect(url_for('views.home'))
     return render_template('user_form.html', user=current_user)
@@ -38,7 +39,7 @@ def signup():
         name = request.form.get('name')
         email = request.form.get('email')
         passw = request.form.get('password')
-        
+
         user = User.query.filter_by(email=email).first()
 
         if user:
--- AmarDaan/views.py
+++ AmarDaan/views.py
@@ -3,11 +3,13 @@ from flask_login import login_required, current_user
 from .models import Campaign
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from sslcommerz_lib import SSLCOMMERZ
 import os
 import time
 import smtplib
 from email.mime.text import MIMEText
 import random
+
 views = Blueprint('views', __name__)
 
 
@@ -15,6 +17,7 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template('index.html', user=current_user)
+
 
 @views.route('/dashboard', methods=["POST", "GET"])
 @login_required
@@ -29,6 +32,7 @@ def dashboard():
         db.session.commit()
         print("Note added successfully!")
     return render_template('statistics.html', user=user, campaigns=user_campaigns)
+
 
 @views.route('/campaign', methods=["GET", "POST"])
 def campaign():
@@ -53,7 +57,7 @@ def campaign():
                 upload_folder = os.path.join(current_app.root_path, 'static', current_app.config['UPLOAD_FOLDER'])
                 os.makedirs(upload_folder, exist_ok=True)
                 filename = os.path.join(upload_folder, photo.filename)
-                photo.save(filename)      
+                photo.save(filename)
 
             camp_gender = request.form.get('camp_gender')
             camp_age = request.form.get('camp_age')
@@ -87,6 +91,7 @@ def campaign():
 
     return render_template('campaign.html', user=user)
 
+
 @views.route('/upload_profile_picture', methods=["POST"])
 @login_required
 def upload_profile_picture():
@@ -108,10 +113,11 @@ def upload_profile_picture():
         user.profile_picture = relative_path
         user.profile_picture_updated = time.time()  # Set to the current timestamp
         db.session.commit()
-        
+
         print("Profile Picture Updated Successfully!")
 
     return redirect(url_for('views.user_profile'))
+
 
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
@@ -123,6 +129,7 @@ def send_email(subject, body, sender, recipients, password):
         smtp_server.sendmail(sender, recipients, msg.as_string())
     print("Message Sent!")
 
+
 def otp_verification():
     otp = random.randint(pow(10, 5), (pow(10, 6) - 1))
     subject = "Your OTP Verification Code"
@@ -132,6 +139,7 @@ def otp_verification():
     password = "ecaflcbwzegogtnr"
     send_email(subject, body, sender, recipients, password)
     return otp
+
 
 @views.route('/update_profile', methods=["POST"])
 @login_required
@@ -174,7 +182,7 @@ def otp_verified():
                 user.email = email
             if password:
                 user.password = generate_password_hash(password=password, method='sha256')
-            if facebook:    
+            if facebook:
                 user.facebook = f"https://www.fb.com/{facebook}"
             if instagram:
                 user.instagram = f"https://www.instagram.com/{instagram}"
@@ -188,6 +196,7 @@ def otp_verified():
     else:
         return render_template("error.html", message="ERROR 404 - WRONG OTP")
 
+
 @views.route('/statistics')
 @login_required
 def statistics():
@@ -198,10 +207,84 @@ def statistics():
 def profile():
     return render_template('user_profile.html', user=current_user)
 
+
 @views.route('/campaign_details')
 def campaign_details():
     return render_template('campaign_details.html', user=current_user)
 
+
 @views.route('/faq')
 def faq():
     return render_template('faq.html', user=current_user)
+
+
+@views.route('/payment_gateway', methods=["POST", "GET"])
+@login_required
+def payment_gateway():
+    settings = {'store_id': 'amard65277e36db8a5', 'store_pass': 'amard65277e36db8a5@ssl', 'issandbox': True}
+    sslcz = SSLCOMMERZ(settings)
+    post_body = {}
+    post_body['total_amount'] = 100.26
+    post_body['currency'] = "BDT"
+    post_body['tran_id'] = "12345"
+    post_body['success_url'] = "your success url"
+    post_body['fail_url'] = "your fail url"
+    post_body['cancel_url'] = "your cancel url"
+    post_body['emi_option'] = 0
+    post_body['cus_name'] = "test"
+    post_body['cus_email'] = "test@test.com"
+    post_body['cus_phone'] = "01700000000"
+    post_body['cus_add1'] = "customer address"
+    post_body['cus_city'] = "Dhaka"
+    post_body['cus_country'] = "Bangladesh"
+    post_body['shipping_method'] = "NO"
+    post_body['multi_card_name'] = ""
+    post_body['num_of_item'] = 1
+    post_body['product_name'] = "Test"
+    post_body['product_category'] = "Test Category"
+    post_body['product_profile'] = "general"
+
+    response = sslcz.createSession(post_body)
+    print(response)
+
+
+@views.route('/payment_verification', methods=["POST", "GET"])
+def payment_verification():
+    settings = {'store_id': 'amard65277e36db8a5', 'store_pass': 'amard65277e36db8a5@ssl', 'issandbox': True}
+    sslcz = SSLCOMMERZ(settings)
+    post_body = {}
+    post_body['tran_id'] = '5E121A0D01F92'
+    post_body['val_id'] = '200105225826116qFnATY9sHIwo'
+    post_body['amount'] = "10.00"
+    post_body['card_type'] = "VISA-Dutch Bangla"
+    post_body['store_amount'] = "9.75"
+    post_body['card_no'] = "418117XXXXXX6675"
+    post_body['bank_tran_id'] = "200105225825DBgSoRGLvczhFjj"
+    post_body['status'] = "VALID"
+    post_body['tran_date'] = "2020-01-05 22:58:21"
+    post_body['currency'] = "BDT"
+    post_body['card_issuer'] = "TRUST BANK, LTD."
+    post_body['card_brand'] = "VISA"
+    post_body['card_issuer_country'] = "Bangladesh"
+    post_body['card_issuer_country_code'] = "BD"
+    post_body['store_id'] = "test_testemi"
+    post_body['verify_sign'] = "d42fab70ae0bcbda5280e7baffef60b0"
+    post_body[
+        'verify_key'] = "amount,bank_tran_id,base_fair,card_brand,card_issuer,card_issuer_country,card_issuer_country_code,card_no,card_type,currency,currency_amount,currency_rate,currency_type,risk_level,risk_title,status,store_amount,store_id,tran_date,tran_id,val_id,value_a,value_b,value_c,value_d"
+    post_body['verify_sign_sha2'] = "02c0417ff467c109006382d56eedccecd68382e47245266e7b47abbb3d43976e"
+    post_body['currency_type'] = "BDT"
+    post_body['currency_amount'] = "100.00"
+    post_body['currency_rate'] = "1.0000"
+    post_body['base_fair'] = "0.00"
+    post_body['value_a'] = ""
+    post_body['value_b'] = ""
+    post_body['value_c'] = ""
+    post_body['value_d'] = ""
+    post_body['risk_level'] = "0"
+    post_body['risk_title'] = "Safe"
+    if sslcz.hash_validate_ipn(post_body):
+        response = sslcz.validationTransactionOrder(post_body['val_id'])
+        print("inside payment verification")
+        print(response)
+    else:
+        print("Hash validation failed")
